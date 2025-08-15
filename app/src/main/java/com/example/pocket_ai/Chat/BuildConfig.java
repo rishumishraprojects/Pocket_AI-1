b7e24d433e3d0dfcb9736c112ea9e1955babadad
Rishu Mishra <rishumishraoffcial1507@gmail.com>
2025-08-15 23:58:23 +0530

    Update BuildConfig.java

--- app/src/main/java/com/example/pocket_ai/Chat/BuildConfig.java
+++ app/src/main/java/com/example/pocket_ai/Chat/BuildConfig.java
@@ -1,5 +1,5 @@
 package com.example.pocket_ai.Chat;
 
 public class BuildConfig {
-    public static String apiKey = "AIzaSyBgDYtuF5kgJbn0d1tBp1GK71I9E-pDJ3c";
+    public static String apiKey = "Enter your own API-KEY";
 }
